92a29acc688de259c4ed9477ab2cfaa246db465d
azka <kkazka06@gmail.com>
2026-06-01 00:17:39 +0700

    Refactor calendar component to use Chevron icons

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,10 +54,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
+        Chevron: ({ className, ...props }) => (
           <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
         ),
-        IconRight: ({ className, ...props }) => (
+        Chevron: ({ className, ...props }) => (
           <ChevronRight className={cn("h-4 w-4", className)} {...props} />
         ),
       }}
